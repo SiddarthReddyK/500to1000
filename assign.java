de45cce4d374c7cf38e625785f06a2297ca5ae76
SiddarthReddyK <siddarthkun2006@gmail.com>
2025-08-12 21:17:18 +0530

    codechef3

---
+++ assign.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class assign
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int x = sc.nextInt();
+            if(x+3<=10) System.out.println("Yes");
+            else System.out.println("No");
+        }
+	}
+}
