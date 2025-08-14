e50de6f466244d703191c2e975c8b4d030844be2
SiddarthReddyK <siddarthkun2006@gmail.com>
2025-08-14 22:25:32 +0530

    codechef 9

---
+++ sale.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class sale
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            int x = sc.nextInt();
+            if(x<=100) System.out.println(x);
+            else if(x>100 && x<=1000) System.out.println(x-25);
+            else if(x>1000 && x<=5000) System.out.println(x-100);
+            else System.out.println(x-500);
+        }
+	}
+}
